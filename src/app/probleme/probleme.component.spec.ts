909b0a1c214740c139660e5ba1b1a99929f4a19e
Thomas Vo <1273590@cmontmorency.qc.ca>
2023-03-10 16:48:18 -0500

    Test Zone PRÉNOM valide avec 3 caractères

---
+++ src/app/probleme/probleme.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ProblemeComponent } from './probleme.component';
+
+describe('ProblemeComponent', () => {
+  let component: ProblemeComponent;
+  let fixture: ComponentFixture<ProblemeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [ ProblemeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProblemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Zone PRÉNOM valide avec 3 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(3))
+    expect(zone.valid).toBeTruthy();
+  });
+});
