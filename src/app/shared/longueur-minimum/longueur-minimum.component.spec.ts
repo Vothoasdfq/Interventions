22765570f750ddd2c8c2dc7bdb9bcadb62f39010
Thomas Vo <1273590@cmontmorency.qc.ca>
2023-03-17 15:55:14 -0400

    Test prenom #9 | Une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide

--- src/app/shared/longueur-minimum/longueur-minimum.component.spec.ts
+++ src/app/shared/longueur-minimum/longueur-minimum.component.spec.ts
@@ -16,4 +16,11 @@ describe('', () => {
         expect(result).toBeNull;
     });
 
+    it('#9 | Une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide', () => {
+        let control = { value: '   je le veux   '.repeat(1) }
+        let validatorFn =  VerifierCaracteresValidator.longueurMinimum(3);
+        let result= validatorFn(control as AbstractControl);
+        expect(result).toBeNull;
+    });
+
 });
