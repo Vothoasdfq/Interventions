a1df3f4a6380884b57591feb0bd52b7e1326a705
Thomas Vo <1273590@cmontmorency.qc.ca>
2023-03-31 16:41:58 -0400

    Test #22 | Zone ADRESSE COURRIEL est invalide sans valeur quand notifier par courriel

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -108,13 +108,45 @@ describe('ProblemeComponent', () => {
 
     let zone = component.problemeForm.get('courrielGroup.courriel');
     expect(zone.enabled).toBeTruthy();
-});
+  });
 
-it('#21 | Zone CONFIRMER COURRIEL est activée quand notifier par courriel', () => {
+  it('#21 | Zone CONFIRMER COURRIEL est activée quand notifier par courriel', () => {
+      component.appliquerNotifications("parCourriel");
+
+      let zone = component.problemeForm.get('courrielGroup.courrielConfirmation');
+      expect(zone.enabled).toBeTruthy();
+  });
+
+  it('#22 | Zone ADRESSE COURRIEL est invalide sans valeur quand notifier par courriel', () => {
     component.appliquerNotifications("parCourriel");
 
-    let zone = component.problemeForm.get('courrielGroup.courrielConfirmation');
-    expect(zone.enabled).toBeTruthy();
-});
+    let zone = component.problemeForm.get('courrielGroup.courriel');
+    zone.setValue('');
+    expect(zone.valid).toBeFalsy();
+  });
+
+  it('#23 | Zone CONFIRMER COURRIEL est invalide sans valeur quand notifier par courriel', () => {
+    
+  });
+
+  it('#24 | Zone ADRESSE COURRIEL est invalide avec un format non conforme', () => {
+    
+  });
+
+  it('#25 | Zone ADRESSE COURRIEL sans valeur et Zone CONFIRMER COURRIEL avec valeur valide retourne null', () => {
+    
+  });
+
+  it('#26 | Zone ADRESSE COURRIEL avec valeur valide et Zone CONFIRMER COURRIEL sans valeur retourne null', () => {
+    
+  });
+
+  it('#27 | Zones ADRESSE COURRIEL et CONFIRMER COURRIEL sont invalides si les valeurs sont différentes quand notifier par courriel', () => {
+    
+  });
+
+  it('#28 | Zones ADRESSE COURRIEL et CONFIRMER COURRIEL sont valides si les valeurs sont identiques quand notifier par courriel', () => {
+    
+  });
   
 });
